c80d90d6b0c0f3ee2b138da354a5e02e05b5baac
kmolnar <42022497+kmolnar@users.noreply.github.com>
2018-08-27 12:44:45 +1000

    Version bump

--- src/fixate/__init__.py
+++ src/fixate/__init__.py
@@ -2,7 +2,7 @@ from fixate.config import CONFIG_LOADED, RESOURCES
 from fixate.config.local_config import setup_config
 from .__main__ import run_main_program
 
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 
 if not CONFIG_LOADED:
     setup_config()
