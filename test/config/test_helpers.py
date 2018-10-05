9807047ff342a95e8ef0205e4ac3281d811e48d9
Ryan Parry-Jones <ryanspj@gmail.com>
2018-10-05 10:25:13 +1000

    Added test for overriding config
    Loads two separate configuration yamls.
    test_val1 should be overrided
    test_val2 should be intact from first config
    test_val3 should be newly added

--- test/config/test_helpers.py
+++ test/config/test_helpers.py
@@ -38,6 +38,15 @@ def test_config_yaml_loader_file_path(clean_config):
     assert conf.test_val1 == "Hello"
 
 
+def test_config_yaml_override_config(clean_config):
+    conf = clean_config
+    conf.load_yaml_config("test_val1: Hello\ntest_val2: World")
+    conf.load_yaml_config("test_val1: Hi\ntest_val3: New")
+    assert conf.test_val1 == "Hi"
+    assert conf.test_val2 == "World"
+    assert conf.test_val3 == "New"
+
+
 def test_clean_config_fixture_working(clean_config):
     conf = clean_config
     with pytest.raises(AttributeError):
