24b4a16736c80b28f40aeb52972c52f9654983ef
Ryan Parry-Jones <rparry-jones@ampcontrol.com.au>
2018-10-09 10:45:19 +1100

    Version Bump for release
    Release for some improvements for first production deployment.

--- src/fixate/__init__.py
+++ src/fixate/__init__.py
@@ -2,6 +2,6 @@ from fixate.config import RESOURCES
 from fixate.config.local_config import setup_config
 from .__main__ import run_main_program
 
-__version__ = '0.3.5'
+__version__ = '0.4.0'
 
 setup_config()
