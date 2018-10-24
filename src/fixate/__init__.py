2f9f52efc37b9427278506b239b802103e0eade3
Ryan Parry-Jones <ryanspj@gmail.com>
2018-10-25 10:56:56 +1100

    Version Bump
    Preparation for release

--- src/fixate/__init__.py
+++ src/fixate/__init__.py
@@ -2,6 +2,6 @@ from fixate.config import RESOURCES
 from fixate.config.local_config import setup_config
 from .__main__ import run_main_program
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 
 setup_config()
