7251b31b5e9f0e123d813f6655cd4046135a52db
Clint Lawrence <clint.lawrence@gmail.com>
2019-02-24 20:39:30 +1100

    Continue to add functionality and improve the config util

--- scripts/fxconfig_shell.py
+++ scripts/fxconfig_shell.py
@@ -2,7 +2,7 @@ import cmd2
 import argparse
 import visa
 import json
-from fxconfig import backup_file, visa_id_query, FxConfigError
+from fxconfig import backup_file, visa_id_query, FxConfigError, serial_id_query
 from pyvisa.errors import VisaIOError
 
 
@@ -16,16 +16,17 @@ Commands
 fx> list existing                           # show the entries from config file
 fx> list visa                               # show resources returned by visa_resources()
 fx> list updated                            # show what will be saved modified state
-fx> add visa serial <port> [<baudrate>]
-fx> add visa tcp <host | ip address>
-fx> add serial bk176x <port> <baudrate>
+fx> add visa-serial <port> [<baudrate>]
+fx> add visa-tcp <host | ip address>
+fx> add serial-bk176x <port> <baudrate>
+fx> add visa list                           # print numbered show resources and user can enter a number.
 fx> test existing                           # idn everything in existing config and report
 fx> test updated                            # idn everything in updated config and report
 fx> save                                    # replace existing with updated. existing will be first copied to *.bak
 
 nice to have:
-fx> test visa serial ...
-fx> test visa tcp
+fx> test visa serial <port> [<baudrate>]
+fx> test visa tcp <host | ip address>
 fx> test serial
 
 """
@@ -45,13 +46,29 @@ list_parser.add_argument('type', choices=choices)
 test_parser = argparse.ArgumentParser(prog='test')
 test_parser.add_argument('type', choices=choices)
 
+# add_parser = argparse.ArgumentParser(prog='add')
+# add_visa = add_parser.add_subparsers()
+# add_visa.
+# add_parser.add_argument('')
+
 
 class FxConfigCmd(cmd2.Cmd):
+    prompt = "fx>"
+
+
     def __init__(self):
         super().__init__()
         self.config_file_path = None
         self.existing_config_dict = None
         self.updated_config_dict = None
+
+        # Enable file-system path completion for the save and open commands
+        self.complete_save = self.path_complete
+        self.complete_open = self.path_complete
+
+    def postloop(self):
+        # Print a new line so the shell prompt get printed on a it's own line after we exit
+        self.poutput("")
 
     @cmd2.with_argparser(list_parser)
     def do_list(self, args):
@@ -73,24 +90,10 @@ class FxConfigCmd(cmd2.Cmd):
     @cmd2.with_argparser(test_parser)
     def do_test(self, args):
         if args.type == "existing":
-            visa_resources = self.existing_config_dict["INSTRUMENTS"]["visa"]
-            serial_resources = self.existing_config_dict["INSTRUMENTS"]["serial"]
-            for idn, visa_resource_name in visa_resources:
-                try:
-                    new_idn = visa_id_query(visa_resource_name)
-                except (VisaIOError, FxConfigError):
-                    self._test_print_error(visa_resource_name, "Error opening or responding to IDN")
-                else:
-                    if new_idn.strip() == idn.strip():
-                        self._test_print_ok(visa_resource_name, idn.strip())
-                    else:
-                        self._test_print_error(visa_resource_name, "IDN Response does not match")
-            # TODO: add serial resources
+            self._test_config_dict(self.existing_config_dict)
 
         elif args.type == "updated":
-            self.poutput("test updated")
-            # TODO: Implement updated. To do this, we will just create a function/method from
-            # the existing version and pass in the appropriate config_dict
+            self._test_config_dict(self.updated_config_dict)
 
         elif args.type == "visa":
             for visa_resource_name in visa.ResourceManager().list_resources():
@@ -156,6 +159,34 @@ class FxConfigCmd(cmd2.Cmd):
         self.poutput("OK: ", end="", color=GREEN)
         self.poutput(str(name), end="", color=CYAN)
         self.poutput(" - {}".format(msg))
+
+    def _test_config_dict(self, config_dict):
+        visa_resources = config_dict["INSTRUMENTS"]["visa"]
+        serial_resources = config_dict["INSTRUMENTS"]["serial"]
+        for idn, visa_resource_name in visa_resources:
+            try:
+                new_idn = visa_id_query(visa_resource_name)
+            except (VisaIOError, FxConfigError):
+                self._test_print_error(visa_resource_name, "Error opening or responding to IDN")
+            except Exception as e:
+                self.perror(e)
+            else:
+                if new_idn.strip() == idn.strip():
+                    self._test_print_ok(visa_resource_name, idn.strip())
+                else:
+                    self._test_print_error(visa_resource_name, "IDN Response does not match")
+
+        for port, params in serial_resources.items():
+            id, baudrate = params
+            try:
+                new_id = serial_id_query(port, baudrate)
+            except Exception as e:
+                self._test_print_error(e, "Error opening port '{}' or responding to ID query".format(port))
+            else:
+                if new_id.strip() == id.strip():
+                    self._test_print_ok(port, str(params))
+                else:
+                    self._test_print_error(port, "ID query does not match")
 
 
 if __name__ == '__main__':
