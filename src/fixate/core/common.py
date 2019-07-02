5b67e737cd16bfee8db8fdc756f4aefbaa6c6aa1
clint-lawrence <clint.lawrence@gmail.com>
2019-07-02 11:11:02 +1000

    Remove _async_raise which is no longer used.

--- src/fixate/core/common.py
+++ src/fixate/core/common.py
@@ -12,20 +12,6 @@ from fixate.core.exceptions import ParameterError, InvalidScalarQuantityError
 logger = logging.getLogger(__name__)
 
 
-def _async_raise(tid, exctype):
-    """raises the exception, performs cleanup if needed"""
-    if not inspect.isclass(exctype):
-        exctype = type(exctype)
-    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(exctype))
-    if res == 0:
-        raise ValueError("invalid thread id")
-    elif res != 1:
-        # """if it returns a number greater than one, you're in trouble,
-        # and you should call it again with exc=NULL to revert the effect"""
-        ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, None)
-        raise SystemError("PyThreadState_SetAsyncExc failed")
-
-
 UNITS = {
     "Hz",
     "Vpp",
