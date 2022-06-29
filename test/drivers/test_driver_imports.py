f8405cc378680e55ed1afe39a8791011cc0e0c9b
John2202W <93100546+John2202W@users.noreply.github.com>
2022-06-29 11:56:05 +1000

    Remove unused test code
    Remove unused test code and files

--- test/drivers/test_driver_imports.py
+++ test/drivers/test_driver_imports.py
@@ -1,8 +1,10 @@
-# import unittest.mock
-# import sys
-# import os
-# import ctypes
 import pytest
+
+"""These tests are a very minimum smoke test on the drivers.
+
+while we work to get a more complete test in place that includes the
+hardware, these are a minimum check that the module can at least
+be imported"""
 
 
 def test_driver_files():
@@ -11,28 +13,6 @@ def test_driver_files():
     import fixate.drivers.funcgen
     import fixate.drivers.lcr
     import fixate.drivers.pps
-
-
-# _files_path = os.path.join(os.path.dirname(__file__), "files")
-#
-# class MockDAQConfig(unittest.mock.MagicMock):
-#     """Mock the daq config: add path to local .h and load .dll files"""
-#
-#     dot_h_file = os.path.join(_files_path, "NIDAQmx.h")
-#
-#     def get_lib(*args, **kwargs):
-#         """Pass in local .dll to generate DAQ functions"""
-#         sys.path.append(_files_path)
-#         os.environ["PATH"] = _files_path + ";" + os.environ["PATH"]
-#         dll = os.path.join(_files_path, "nicaiu.dll")
-#         DAQlib = ctypes.WinDLL(dll)
-#         DAQlib_variadic = ctypes.CDLL(dll)
-#         return DAQlib, DAQlib_variadic
-#
-# def test_daq_driver_import():
-#     # Mock the config - point to local .h and .dll
-#     sys.modules["PyDAQmx.DAQmxConfig"] = MockDAQConfig()
-#     import fixate.drivers.daq
 
 
 # Loading Pydaqmx will fail with NotImplementedError if the .h file is not found
