fd41ddaa528631a57308780893590676865655e2
Jasper-Harvey0 <jasper.harvey@ampcontrolgroup.com>
2024-01-18 09:41:41 +1100

    Replace os.environ with platform.node

--- src/fixate/config/__init__.py
+++ src/fixate/config/__init__.py
@@ -12,6 +12,8 @@ from fixate.config.helper import (
     get_config_dict,
     render_template,
 )
+
+import platform
 import os.path
 import json
 import dataclasses
@@ -28,13 +30,7 @@ CONFIG_DIRECTORY = Path(platformdirs.site_config_dir("Fixate", False))
 LOG_DIRECTORY = Path(platformdirs.user_log_dir("Fixate", False))
 INSTRUMENT_CONFIG_FILE = CONFIG_DIRECTORY / "instruments.json"
 
-try:
-    COMPUTERNAME = os.environ["COMPUTERNAME"]  # On Windows
-except KeyError:
-    COMPUTERNAME = os.uname().nodename  # On POSIX
-except AttributeError:
-    # Cant get the computer name
-    COMPUTERNAME = "Unknown"
+COMPUTERNAME = platform.node()  # Computer name, or empty string if unavailable.
 
 INSTRUMENTS = []
 RESOURCES = {}
