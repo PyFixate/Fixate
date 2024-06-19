ed21cf7830e92c1527ad8b0235a243d4d7358e80
Clint Lawrence <clint.lawrence@ampcontrolgroup.com>
2024-06-19 15:17:06 +1000

    Remove type def signal stuff from the test script.

--- script.py
+++ script.py
@@ -4,40 +4,13 @@ fit together.
 """
 from __future__ import annotations
 from dataclasses import dataclass, field
-from fixate.core._switching import VirtualMux, JigDriver, MuxGroup, PinValueAddressHandler, VirtualSwitch, Signal, Pin
-
-from typing import TypeVar, Generic, Union, Annotated, Literal
-
-S = TypeVar("S")
-
-
-class VirtualMux(Generic[S]):
-    def __init__(self):
-        self._signal_map: dict[Signal, set[Pin]] = {}
-
-    def __call__(self, signal: S, trigger_update: bool = False) -> None:
-        self.multiplex(signal, trigger_update)
-
-    def multiplex(self, signal: S, trigger_update: bool = False) -> None:
-        print(self._signal_map[signal])
-
-
-MuxOneSigDef = Union[
-    Annotated[Literal["sig1"], ("x0",)],
-    Annotated[Literal["sig2"], ("x1",)],
-    Annotated[Literal["sig3"], ("x0", "x1")],
-]
-
-class MuxOne(VirtualMux[MuxOneSigDef]):
-    pass
-
-@dataclass
-class JigMuxGroup(MuxGroup):
-    mux_one: MuxOne = field(default_factory=MuxOne)
-
-
-class MuxOne(VirtualMux[MuxOneSigDef]):
-    pass
+from fixate import (
+    VirtualMux,
+    JigDriver,
+    MuxGroup,
+    PinValueAddressHandler,
+    VirtualSwitch,
+)
 
 
 class MuxOne(VirtualMux):
@@ -47,6 +20,7 @@ class MuxOne(VirtualMux):
         ("sig2", "x1"),
         ("sig3", "x2"),
     )
+
 
 class MuxTwo(VirtualMux):
     pin_list = ("x3", "x4", "x5")
