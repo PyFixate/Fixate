d7cfb9fbdc79e3783cafd29641520b9f9d8dfa9a
jcollins1983 <jason.collins83@gmail.com>
2024-07-29 20:04:54 +1000

    add list existing test

--- test/core/test_config_util.py
+++ test/core/test_config_util.py
@@ -23,10 +23,25 @@ def config_file_dir():
         yield tmpdir
 
 
+@pytest.fixture
+def open_config_file(test_app):
+    test_app.do_open("src/fixate/config/local_config.json.tmpl")
+    yield test_app
+
+
 def test_open_fxconfig_no_file(test_app):
     with pytest.raises(Exception):
         # there should be no file at the default path at this point.
         test_app.do_open("")
+
+
+@pytest.mark.windows
+def test_new_config_file_default(test_app, capfd):
+    # on windows, the default path should work without elevation.
+    # on mac, the default path required fxconfig to be run with elevated permissions.
+    test_app.do_new("")
+    out = capfd.readouterr()
+    assert (out.out).strip() == f"Config loaded: {config_util.INSTRUMENT_CONFIG_FILE}"
 
 
 def test_new_config_file(test_app, config_file_dir, capfd):
@@ -55,3 +70,12 @@ def test_open_fxconfig(test_app, capfd):
     assert (
         out.out
     ).strip() == f"Config loaded: {'src/fixate/config/local_config.json.tmpl'}"
+
+
+def test_list_existing(test_app, open_config_file, capfd):
+    test_app.do_list("existing")
+    out = capfd.readouterr()
+    print(out.out)
+    assert (
+        out.out
+    ).strip() == "VISA || USB0::0x09C4::0x0400::DG1D144904270::INSTR || RIGOL TECHNOLOGIES,DG1022 ,DG1D144904270,,00.03.00.09.00.02.11\nVISA || ASRL38::INSTR || FLUKE,8846A,3821015,08/02/10-11:53\nVISA || USB0::0x0957::0x17A8::MY52160892::INSTR || AGILENT TECHNOLOGIES,MSO-X 3014A,MY52160892,02.41.2015102200\nSERIAL || COM37 || ['address: 0,checksum: 28,command: 49,model: 6823,serial_number: 3697210019,software_version: 29440,start: 170,', 9600]"
