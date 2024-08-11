c538fbedbe526bf1b119bb10dd85f03fdd5f61b2
jcollins1983 <jason.collins83@gmail.com>
2024-08-11 11:29:06 +1000

    remove capfd

--- test/core/test_config_util.py
+++ test/core/test_config_util.py
@@ -57,32 +57,40 @@ def test_new_config_file_exists(test_app, tmp_path):
         test_app.do_new(tmp_path / "test_config.json")
 
 
-def test_open_fxconfig(test_app, capfd):
+def test_open_fxconfig(test_app):
     test_app.do_open("test/config/instruments.json")
-    out = capfd.readouterr()
-    assert (out.out).strip() == f"Config loaded: {'test/config/instruments.json'}"
-
-
-def test_list_existing(test_app, open_config_file, capfd):
-    test_app.do_list("existing")
-    out = capfd.readouterr()
-    assert (
-        (out.out).strip()
-        == "VISA || USB0::0x09C4::0x0400::DG1D144904270::INSTR || RIGOL TECHNOLOGIES,DG1022 ,DG1D144904270,,00.03.00.09.00.02.11\r\n"
-        "VISA || ASRL38::INSTR || FLUKE,8846A,3821015,08/02/10-11:53\r\n"
-        "VISA || USB0::0x0957::0x17A8::MY52160892::INSTR || AGILENT TECHNOLOGIES,MSO-X 3014A,MY52160892,02.41.2015102200\r\n"
-        "SERIAL || COM37 || ['address: 0,checksum: 28,command: 49,model: 6823,serial_number: 3697210019,software_version: 29440,start: 170,', 9600]"
-    )
+    assert test_app.config_file_path == "test/config/instruments.json"
+    assert test_app.updated_config_dict == {
+        "INSTRUMENTS": {
+            "visa": [
+                [
+                    "RIGOL TECHNOLOGIES,DG1022 ,DG1D144904270,,00.03.00.09.00.02.11\n",
+                    "USB0::0x09C4::0x0400::DG1D144904270::INSTR",
+                ],
+                ["FLUKE,8846A,3821015,08/02/10-11:53\r\n", "ASRL38::INSTR"],
+                [
+                    "AGILENT TECHNOLOGIES,MSO-X 3014A,MY52160892,02.41.2015102200",
+                    "USB0::0x0957::0x17A8::MY52160892::INSTR",
+                ],
+            ],
+            "serial": {
+                "COM37": [
+                    "address: 0,checksum: 28,command: 49,model: 6823,serial_number: 3697210019,software_version: 29440,start: 170,",
+                    9600,
+                ]
+            },
+        }
+    }
 
 
 # testing the _add_visa_to_config method directly since there is too much reliance on the ResourceManager
 # which would need to be monkeypatched or mocked.
-def test_add_visa_to_config_duplicate(test_app, open_config_file, capfd):
+def test_add_visa_to_config_duplicate(test_app, open_config_file):
+    before_add = test_app.updated_config_dict["INSTRUMENTS"]["visa"]
     test_app._add_visa_to_config(
         "FLUKE,8846A,3821015,08/02/10-11:53\r\n", "ASRL38::INSTR"
     )
-    out = capfd.readouterr()
-    assert (out.out).strip() == "Instrument already in config"
+    assert before_add == test_app.updated_config_dict["INSTRUMENTS"]["visa"]
 
 
 def test_add_visa_to_config(test_app, open_config_file):
@@ -96,17 +104,17 @@ def test_add_visa_to_config(test_app, open_config_file):
     assert test_app.updated_config_dict["INSTRUMENTS"]["visa"][-1][1] == "ASRL39::INSTR"
 
 
-def test_add_serial_error(test_app, open_config_file):
+def test_add_serial_error(test_app):
     with pytest.raises(Exception):
         test_app.do_add("serial com38")
 
 
-def test_add_serial_duplicate(test_app, monkeypatch, open_config_file, capfd):
+def test_add_serial_duplicate(test_app, monkeypatch, open_config_file):
     monkeypatch.setattr(
         config_util,
         "serial_id_query",
         lambda x, y: "address: 0,checksum: 28,command: 49,model: 6823,serial_number: 3697210019,software_version: 29440,start: 170,",
     )
+    before_add = test_app.updated_config_dict["INSTRUMENTS"]["serial"]
     test_app.do_add("serial COM37 9600")
-    out = capfd.readouterr()
-    assert out.out.strip() == "Serial port COM37 already in config"
+    assert before_add == test_app.updated_config_dict["INSTRUMENTS"]["serial"]
