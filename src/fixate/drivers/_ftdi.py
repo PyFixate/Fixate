d9c867f6349e42d01e54f825969857405a73fb03
Josh M <josh.marshall@ampcontrolgroup.com>
2024-08-13 13:30:12 +1000

    mac fixes: _ftdi.py using .dylib

--- src/fixate/drivers/_ftdi.py
+++ src/fixate/drivers/_ftdi.py
@@ -3,6 +3,7 @@ This is wrapped privately so it can be ommitted from the documentation build.
 """
 
 import ctypes
+import textwrap
 import sys
 
 if sys.platform == "win32":
@@ -12,6 +13,18 @@ if sys.platform == "win32":
         raise ImportError(
             "Unable to find FTD2XX.dll.\nPlugging in an FTDI device will install the DLL."
         ) from e
+
+elif sys.platform == "darwin":
+    try:
+        ftdI2xx = ctypes.cdll.LoadLibrary("/usr/local/lib/libftd2xx.dylib")
+    except Exception as e:
+        err_msg = textwrap.dedent(
+            """\
+            Unable to find libftd2xx.dylib.
+            Install from https://ftdichip.com/drivers/d2xx-drivers
+            (suggestion: copy libftd2xx.1.4.30.dylib to /usr/local/lib and then symlink libftd2xx.dylib)"""
+        )
+        raise ImportError(err_msg) from e
 
 else:
     try:
