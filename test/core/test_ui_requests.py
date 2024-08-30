c08f41704ab7990e097a2496b780f883f98cbb44
Jason Collins <jason.collins83@gmail.com>
2024-08-30 19:30:58 +1000

    adjust tests to account for movement of logic from ui to ui controller (ui.py)

--- test/core/test_ui_requests.py
+++ test/core/test_ui_requests.py
@@ -1,22 +1,19 @@
 import unittest
 from unittest.mock import MagicMock
 from pubsub import pub
-from fixate.core.ui import _user_req_input, _float_validate, user_serial
+from fixate.core.ui import (
+    _user_req_input,
+    _float_validate,
+    user_serial,
+    user_input_float,
+    _ten_digit_serial,
+)
+from fixate.core.exceptions import UserInputError
 
 
 class MockUserDriver(MagicMock):
-    def execute_target(self, msg, q, target=None, attempts=5, kwargs=None):
-        if target:
-            try:
-                if self.test_value is None:
-                    ret_val = target(**kwargs)
-                else:
-                    ret_val = target(self.test_value, **kwargs)
-                q.put(("Result", ret_val))
-            except Exception as e:
-                q.put(("Exception", e))
-        else:
-            q.put(("Result", self.test_value))
+    def execute_target(self, msg, q):
+        q.put(self.test_value)
 
 
 class TestUserRequest(unittest.TestCase):
@@ -36,28 +33,34 @@ class TestUserRequest(unittest.TestCase):
         self.mock.return_value = "World"
         self.assertEqual(self.test_method("HI", target=self.mock), ("Result", "World"))
 
+    def test_float_validate_fails(self):
+        # _float_validate is tested implicitly by user_input_float, but because of how the
+        # failures are done at that level, we should check the failure case here
+        self.assertFalse(_float_validate("abc"))
+
     def test_target_float(self):
         self.mock.test_value = "1.23"
-        resp = self.test_method("message", target=_float_validate)
+        self.test_method = user_input_float
+        resp = self.test_method("message")
         self.assertAlmostEqual(resp[1], float(self.mock.test_value))
 
     def test_target_float_fails(self):
         self.mock.test_value = "abc"
-        resp = self.test_method("message", target=_float_validate)
-        self.assertFalse(resp[1])
+        self.test_method = user_input_float
+        resp = self.test_method("message")
+        self.assertTrue(isinstance(resp[1], UserInputError))
 
     def test_user_serial(self):
         self.mock.test_value = "1234567890"
         resp = user_serial("message")
         self.assertEqual(resp[1], int(self.mock.test_value))
 
+    def test_ten_digit_serial_fails(self):
+        # _ten_digit_serial is tested implicitly by user_serial since it's the default, but because of how the
+        # failures are done at that level, we should check the failure case here
+        self.assertEqual(_ten_digit_serial("123456789"), False)
+
     def test_user_serial_fail(self):
         self.mock.test_value = "123456789"  # < 10 digits
         resp = user_serial("message")
-        self.assertFalse(resp[1])
-
-    def test_user_serial_no_target(self):
-        # Not really meaningful test?
-        self.mock.test_value = 123
-        resp = user_serial("message", None)
-        self.assertEqual(resp[1], self.mock.test_value)
+        self.assertTrue(isinstance(resp[1], UserInputError))
