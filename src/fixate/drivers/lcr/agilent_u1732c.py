966d0bc4aa458528b95fbe4351415022f27b802e
Jasper-Harvey0 <jasper.harvey@ampcontrolgroup.com>
2026-01-22 11:54:20 +1100

    Fix formatting

--- src/fixate/drivers/lcr/agilent_u1732c.py
+++ src/fixate/drivers/lcr/agilent_u1732c.py
@@ -79,7 +79,9 @@ FETC? ALL
 
 
 class AgilentU1732C(LCR):
-    REGEX_ID = "(Keysight|Agilent) Technologies,U1732C" # Regex allows multiple manufacturers
+    REGEX_ID = (
+        "(Keysight|Agilent) Technologies,U1732C"  # Regex allows multiple manufacturers
+    )
     INSTR_TYPE = "VISA"
 
     def __init__(self, instrument):
