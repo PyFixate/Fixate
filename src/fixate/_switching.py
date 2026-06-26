976b2ffbc9250982623262023a6a72e38c4746ed
Daniel Montanari <Daniel.Montanari@ampcontrolgroup.com>
2026-06-26 12:14:27 +1000

    default type annotations cause syntax errors in 3.12

--- src/fixate/_switching.py
+++ src/fixate/_switching.py
@@ -47,13 +47,13 @@ from operator import or_
 
 type Signal = str
 type EmptySignal = Literal[""]
-type MuxSignal[M: Signal = Signal] = M | EmptySignal
 type Pin = str
 type PinList = Sequence[Pin]
 type PinSet = frozenset[Pin]
-type SignalMap[S: Signal = Signal] = dict[S, PinSet]
-type TreeDef[S: Signal = Signal] = Sequence[S | "TreeDef"]
 type PinUpdateCallback = Callable[[PinUpdate, bool], None]
+type MuxSignal[M: Signal] = M | EmptySignal
+type SignalMap[S: Signal] = dict[S, PinSet]
+type TreeDef[S: Signal] = Sequence[S | "TreeDef"]
 
 
 @dataclass(frozen=True)
